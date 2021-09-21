97206ef66a6049f30bf23bb46b83e2dc35c334a2
Mehmet <58147869+koti42@users.noreply.github.com>
2021-09-21 11:30:18 +0300

    Profil Düzenleme Tamamlandı

--- resources/views/admin/component/sidebar.blade.php
+++ resources/views/admin/component/sidebar.blade.php
@@ -10,7 +10,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('back/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('back/images/Profiles').'/'.\Illuminate\Support\Facades\Auth::user()->ProfilePicture}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="{{route('profiles')}}" class="d-block">{{\Illuminate\Support\Facades\Auth::user()->name}}</a>
